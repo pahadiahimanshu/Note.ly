511724c6ed3c81d38c89ca145442efed1c1dc377
Himanshu Pahadia <himanshu14045>
2016-10-03 00:35:40 +0530

    Shared preferences addded

--- app/src/main/java/com/example/himanshu/notely/MainActivity.java
+++ app/src/main/java/com/example/himanshu/notely/MainActivity.java
@@ -14,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     String t;
 
-    private Button userName;
+    private Button muserName;
+    private Button mloginButton;
+    private Button mNoteButton;
 
     private TextView editName;
     private EditText nameEditText;
@@ -23,16 +25,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        userName = (Button) findViewById(R.id.userName);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        muserName = (Button) findViewById(R.id.userName);
+        mloginButton = (Button) findViewById(R.id.loginButton);
+        mNoteButton = (Button) findViewById(R.id.noteButton);
 
         editName = (TextView)findViewById(R.id.editName);
         nameEditText = (EditText)findViewById(R.id.nameEditText);
 
         t = new String();
         t = "RESULT APP";
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.himanshu.notely", Context.MODE_PRIVATE );
+
+        final SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.himanshu.notely", Context.MODE_PRIVATE );
 
         String username = sharedPreferences.getString("username","");
 
@@ -41,11 +48,24 @@ public class MainActivity extends AppCompatActivity {
             Log.i(t,"The username from preferences is -"+username);
             editName.setText("Hi "+username+"!");
         }
+        else
+        {
+            Log.i(t,"EMPTY USERAME");
+        }
 
-        userName.setOnClickListener(new View.OnClickListener() {
+        muserName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
+                Log.i(t,"inside on click");
+                String s = nameEditText.getText().toString();
+                if(s.equals("Enter name")==false)
+                {
+                    Log.i(t,"string inside name edit - "+s);
+                    sharedPreferences.edit().putString("username",s).apply();
+                    editName.setText("Hi "+s+"!");
+                }
+
 
             }
         });
